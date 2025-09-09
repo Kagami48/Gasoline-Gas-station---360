3b746eb8245570477b61be3a9250d0bdfc35b384
Kagami48 <aloraini.slman@gmail.com>
2025-09-09 05:11:15 +0300

    gasoline

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-panorama",
-      "name": "Panorama",
+      "id": "0-",
+      "name": "الموتيل",
       "levels": [
         {
           "tileSize": 256,
@@ -16,15 +16,213 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7992487261711467,
+          "pitch": -0.03254464940623336,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "مظلة الوقود",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3067031776067175,
+          "pitch": -0.07667908907417953,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -2.4903610714998,
+          "pitch": -0.07803313967695757,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -2.806176257139988,
+          "pitch": -0.06730066775325838,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.1449869526684697,
+          "pitch": -0.15426000169473753,
+          "rotation": 0,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "ساحة المسجد",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6133801795454694,
+          "pitch": -0.06411228391216639,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": 2.9703831745245575,
+          "pitch": -0.17576618770243435,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -2.4622988579883938,
+          "pitch": -0.06247624073870384,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "المجمع التجاري",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "مدخل المحطة",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.13169423049730788,
+        "pitch": 0,
+        "fov": 1.0841904824255306
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0914124959310243,
+          "pitch": -0.08136877442669643,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": 0.21091919713355978,
+          "pitch": -0.05105365782368665,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -0.31257843014006426,
+          "pitch": -0.04931588114653351,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
       "infoHotspots": []
     }
   ],
